85885ff412a4190b1d796564784aa4a088f882b3
dani-pinheiro01 <99225514+dani-pinheiro01@users.noreply.github.com>
2022-02-16 22:24:49 -0300

    Add files via upload
    códigos simples e de fácil leitura.

---
+++ media-anual.py
@@ -0,0 +1,12 @@
+print('Média bimestral')
+
+while (media>0):
+
+nota1 = float(input('Digite a nota da avaliação 1:'))
+nota2 = float(input('Digite a nota da avaliação 2:'))
+nota3 = float(input('Digite a nota da avaliação 3:'))
+nota4 = float(input('Digite a nota da avaliação 4:'))
+
+media = (nota1*2+nota2*2+nota3*2+nota4*2)/3
+
+print('A média final é:', media)
---
+++ soma.py
@@ -0,0 +1,6 @@
+numero1 = int(input('numero1:'))
+numero2 = int(input('numero2:'))
+
+resultado = (numero1+numero2)
+
+print('O resultado da soma é:', resultado)
