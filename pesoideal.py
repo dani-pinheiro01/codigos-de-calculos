5989db82f302140a97c633f7b17d966d2f5f9096
Daniel Pinheiro <99225514+dani-pinheiro01@users.noreply.github.com>
2022-02-21 23:04:53 -0300

    Update pesoideal.py

--- pesoideal.py
+++ pesoideal.py
@@ -1,4 +1,4 @@
-sexo = input('Qual seu sexo? Masculino(1) ou Feminino(2):')
+sexo = input('Qual seu sexo? Masculino(M) ou Feminino(F):')
 altura = input('Qual sua altura?')
 
 peso_ideal_M = (72.7*float(altura))-58
